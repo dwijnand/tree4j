8b65ca8c16fdcc4c6b28b656250f7b316df046e5
Dale Wijnand <dale.wijnand@gmail.com>
2010-10-27 00:47:04 +0200

    Initial check-in of tree, added: pom.xml, .hgignore, Tree and MultimapTree
    The POM defines a simple jar project, with some initial dependencies and
    .hgignore ignores eclipse files and directories and maven's target directory.
    
    The Tree interface defines a tree structure, complete of javadoc, while the
    MultimapTree class defines an implementation of Tree.

---
+++ src/main/java/org/bitbucket/dwijnand/tree/MultimapTree.java
@@ -0,0 +1,144 @@
+package org.bitbucket.dwijnand.tree;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Map;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Ordering;
+import com.google.common.collect.TreeMultimap;
+
+/**
+ * A mutable {@link Tree} backed by a {@link Multimap}.
+ * 
+ * @param <T> the type of the nodes in the tree
+ */
+public class MultimapTree<T> implements Tree<T> {
+
+    /**
+     * The parent-children relationships of the tree.
+     */
+    private final Multimap<T, T> nodes;
+
+    /**
+     * The child-parent relationships of the tree.
+     */
+    private final Map<T, T> parents = Maps.newHashMap();
+
+    /**
+     * The root of the tree.
+     */
+    private T root;
+
+    /**
+     * Creates a {@link MultimapTree} whose nodes are ordered according to the
+     * natural ordering of type T.
+     * 
+     * @param <T> the type of the nodes in the new tree
+     * @return a new {@code MultimapTree}
+     */
+    public static <T extends Comparable<T>> MultimapTree<T> create() {
+        return new MultimapTree<T>(Ordering.<T> natural());
+    }
+
+    /**
+     * Creates a {@link MultimapTree} whose nodes are ordered according to the
+     * specified {@link Comparator}.
+     * 
+     * @param <T> the type of the nodes in the new tree
+     * @param comparator the
+     * @return a new {@code MultimapTree}
+     * @throws NullPointerException if the specified comparator is null
+     */
+    public static <T> MultimapTree<T> create(final Comparator<T> comparator) {
+        return new MultimapTree<T>(checkNotNull(comparator));
+    }
+
+    private MultimapTree(final Comparator<T> comparator) {
+        nodes = TreeMultimap.create(comparator, comparator);
+    }
+
+    @Override
+    public boolean contains(final Object o) {
+        if (o == null) {
+            return false;
+        } else if (o == root) {
+            return true;
+        } else {
+            return parents.containsValue(o);
+        }
+    }
+
+    @Override
+    public T getParent(final T node) {
+        return parents.get(checkNotNull(node));
+    }
+
+    @Override
+    public Collection<T> getChildren(final T node) {
+        return nodes.get(checkNotNull(node));
+    }
+
+    @Override
+    public T getRoot() {
+        return root;
+    }
+
+    @Override
+    public void setRoot(final T node) {
+        clear();
+        root = node;
+    }
+
+    @Override
+    public boolean add(final T parent, final T child) {
+        checkNotNull(parent);
+        checkNotNull(child);
+        nodes.put(parent, child);
+        parents.put(child, parent);
+        return true;
+    }
+
+    @Override
+    public void clear() {
+        nodes.clear();
+        parents.clear();
+    }
+
+    @Override
+    public void remove(final T node) {
+        if (node == root) {
+            clear();
+            return;
+        }
+        checkNotNull(node);
+
+        final Collection<T> children = nodes.get(node);
+        // TODO Explain why we need to copy the children into an ImmutableList
+        for (final T child : ImmutableList.copyOf(children)) {
+            remove(child);
+        }
+
+        nodes.removeAll(node);
+        nodes.get(getParent(node)).remove(node);
+        parents.remove(node);
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+}
---
+++ src/main/java/org/bitbucket/dwijnand/tree/Tree.java
@@ -0,0 +1,94 @@
+package org.bitbucket.dwijnand.tree;
+
+import java.util.Collection;
+import java.util.Comparator;
+
+/**
+ * An object that represents a tree structure, or in graph-theory an
+ * arborescence. Specifically it is an acyclic directed graph with a root node.
+ * Each non-root node of the {@link Tree} has a parent and zero or more
+ * children. The order of the children of each node is either natural ordering
+ * or determined by a specified {@link Comparator}.
+ * <p>
+ * {@link Tree} will not accept {@code null} as a node in any of its methods,
+ * throwing a {@link NullPointerException} if a null is passed.
+ * 
+ * @param <T> the type of the nodes in the tree
+ */
+public interface Tree<T> {
+
+    // Query methods
+
+    /**
+     * Returns {@code true} if the {@link Tree} contains the specified node.
+     * 
+     * @param node node to check for
+     * @return {@code true} if the tree contains the specified node.
+     */
+    boolean contains(Object node);
+
+    /**
+     * Returns the parent of the specified node.
+     * 
+     * @param node node on which to get the parent
+     * @return the parent of the specified node
+     */
+    // TODO Define what getParent does if the Tree doesn't contain the node
+    T getParent(T node);
+
+    /**
+     * Returns a collection view of the children for the specified node. If the
+     * specified node doesn't have any children, an empty collection is
+     * returned.
+     * 
+     * @param node node on which to get the children
+     * @return the children of the specified node
+     */
+    // TODO Define what getChildren does if the Tree doesn't contain the node
+    Collection<T> getChildren(T node);
+
+    /**
+     * Returns the root of the {@link Tree}.
+     * 
+     * @return the root of the {@link Tree}
+     */
+    T getRoot();
+
+    // Adding methods
+
+    /**
+     * Removes all the nodes in the {@link Tree} and sets the specified node as
+     * the root.
+     * 
+     * @param node node to set as the new root
+     */
+    void setRoot(T node);
+
+    /**
+     * Adds the specified child node to the {@link Tree}, associated to the
+     * specified parent node. Returns {@code true} if the {@link Tree} changed.
+     * 
+     * @param parent node on which to associate the child node
+     * @param child node to add
+     * @return {@code true} if the {@link Tree} changed
+     */
+    // TODO Define what add does if the Tree doesn't contain the parent
+    // TODO Define what add does if the Tree doesn't contain the child
+    boolean add(T parent, T child);
+
+    // Removing methods
+
+    /**
+     * Removes all the nodes in the {@link Tree}.
+     */
+    void clear();
+
+    /**
+     * Removes the specified node from the {@link Tree}.
+     * 
+     * @param node node to remove
+     */
+    // TODO Define what remove does to the children of the node
+    void remove(T node);
+
+}
