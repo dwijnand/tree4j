2e2c0c986f9b022cdd95adb8256d32bbb1b9291d
Dale Wijnand <dale.wijnand@gmail.com>
2010-10-27 01:35:38 +0200

    MultimapTreeTest: Remove Node and use String as the Tree node type
    This simplifies the Test class, as String has an internal mechanism, which
    enables the user to create a string, without assigning it to a variable, then
    refering to the same string, simply by creating that same sequence of characters.

--- src/test/java/org/bitbucket/dwijnand/tree/MultimapTreeTest.java
+++ src/test/java/org/bitbucket/dwijnand/tree/MultimapTreeTest.java
@@ -2,13 +2,12 @@ package org.bitbucket.dwijnand.tree;
 
 import static org.junit.Assert.assertFalse;
 
-import org.apache.commons.lang.builder.CompareToBuilder;
 import org.junit.Before;
 import org.junit.Test;
 
 public class MultimapTreeTest {
 
-    private Tree<Node> tree;
+    private Tree<String> tree;
 
     @Before
     public void setUp() {
@@ -17,41 +16,20 @@ public class MultimapTreeTest {
 
     @Test
     public void shouldntContainNewlyCreatedNode() {
-        assertFalse(tree.contains(new Node("Dale", 1)));
+        assertFalse(tree.contains("new node"));
     }
 
     @Test
     public void shouldCascadeRemove() {
-        final Node one = new Node("one", 1);
-        tree.setRoot(one);
-        final Node two = new Node("two", 2);
-        final Node three = new Node("tree", 3);
-        tree.add(one, two);
-        tree.add(one, three);
+        tree.setRoot("one");
+        tree.add("one", "two");
+        tree.add("one", "three");
 
-        tree.remove(two);
+        tree.remove("two");
 
-        assertFalse(tree.contains(two));
-        assertFalse(tree.getChildren(one).contains(two));
-        assertFalse(tree.contains(three));
-    }
-
-    private static class Node implements Comparable<Node> {
-
-        public final String name;
-
-        public final Integer value;
-
-        public Node(final String name, final Integer value) {
-            this.name = name;
-            this.value = value;
-        }
-
-        @Override
-        public int compareTo(final Node o) {
-            return CompareToBuilder.reflectionCompare(this, o);
-        }
-
+        assertFalse(tree.contains("two"));
+        assertFalse(tree.getChildren("one").contains("two"));
+        assertFalse(tree.contains("three"));
     }
 
 }
