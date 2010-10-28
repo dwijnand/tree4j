f31e2de21a6a5f932b25fbf2521f2ac0c7304960
Dale Wijnand <dale.wijnand@gmail.com>
2010-10-28 16:18:15 +0200

    MultimapTreeTest: Add 2 contains tests and getChildren test

--- src/test/java/org/bitbucket/dwijnand/tree/MultimapTreeTest.java
+++ src/test/java/org/bitbucket/dwijnand/tree/MultimapTreeTest.java
@@ -1,8 +1,14 @@
 package org.bitbucket.dwijnand.tree;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasItems;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Collection;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,6 +20,19 @@ public class MultimapTreeTest {
     @Before
     public void setUp() {
         tree = MultimapTree.create();
+    }
+
+    @Test
+    public void shouldContainAddedNode() {
+        tree.setRoot("root");
+        tree.add("root", "node");
+        assertTrue(tree.contains("node"));
+    }
+
+    @Test
+    public void shouldContainTheSetRoot() {
+        tree.setRoot("root");
+        assertTrue(tree.contains("root"));
     }
 
     @Test
@@ -38,6 +57,22 @@ public class MultimapTreeTest {
     @Test
     public void shouldntContainNewlyCreatedNode() {
         assertFalse(tree.contains("new node"));
+    }
+
+    @Test
+    public void shouldReturnCorrectChildren() {
+        tree.setRoot("root");
+        tree.add("root", "1");
+        tree.add("1", "1-1");
+        tree.add("1", "1-2");
+        tree.add("1", "1-3");
+        tree.add("root", "2");
+        tree.add("2", "2-1");
+
+        final Collection<String> children = tree.getChildren("1");
+
+        assertThat(3, is(children.size()));
+        assertThat(children, hasItems("1-1", "1-2", "1-3"));
     }
 
     @Test
