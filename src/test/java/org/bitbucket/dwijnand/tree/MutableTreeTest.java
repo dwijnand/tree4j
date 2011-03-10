0940d43aa58f9bb83ccbf027f1c0d6a20b1fc02c
Dale Wijnand <dale.wijnand@gmail.com>
2011-03-10 16:44:33 +0100

    MutableTreeTest: add tests for MutableTree methods

--- src/test/java/org/bitbucket/dwijnand/tree/MutableTreeTest.java
+++ src/test/java/org/bitbucket/dwijnand/tree/MutableTreeTest.java
@@ -1,7 +1,17 @@
 package org.bitbucket.dwijnand.tree;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Collection;
+
 import org.junit.experimental.theories.DataPoints;
 import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
 
 @RunWith(Theories.class)
@@ -12,6 +22,76 @@ public class MutableTreeTest extends TreeTest {
         return new MutableTree[] {MultimapTree.<String> create()};
     }
 
-    // TODO add tests for MutableTree's additional methods
+    @Theory
+    public void setRootShouldSetTheRoot(final MutableTree<String> mutableTree) {
+        mutableTree.setRoot("root");
+
+        assertEquals("root", mutableTree.getRoot());
+    }
+
+    @Theory
+    public void setRootShouldRemoveAllExistingNodes(
+            final MutableTree<String> mutableTree) {
+        mutableTree.setRoot("root");
+        mutableTree.added("root", "child");
+
+        mutableTree.setRoot("new root");
+
+        assertFalse(mutableTree.contains("child"));
+    }
+
+    @Theory
+    public void addedShouldAddTheParentChildAssociation(
+            final MultimapTree<String> multimapTree) {
+        multimapTree.setRoot("root");
+
+        multimapTree.added("root", "child");
+
+        assertTrue(multimapTree.contains("root"));
+        assertTrue(multimapTree.contains("child"));
+        assertTrue(multimapTree.getChildren("root").contains("child"));
+    }
+
+    @Theory
+    public void addedShouldThrowAnIllegalArgumentExceptionOnUnknownParent(
+            final MultimapTree<String> multimapTree) {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException
+                .expectMessage("does not contain parent node unknown parent");
+
+        multimapTree.added("unknown parent", "child");
+    }
+
+    @Theory
+    public void clearShouldRemoveAllNodes(
+            final MultimapTree<String> multimapTree) {
+        multimapTree.setRoot("root");
+        multimapTree.added("root", "child");
+
+        multimapTree.clear();
+
+        assertFalse(multimapTree.contains("root"));
+        assertFalse(multimapTree.contains("child"));
+    }
+
+    @Theory
+    public void removedShouldRemoveTheSpecifiedNodeAndAllOfItsChildren(
+            final MultimapTree<String> multimapTree) {
+        multimapTree.setRoot("root");
+        multimapTree.added("root", "1").added("root", "2")
+                .added("root", "3");
+        multimapTree.added("1", "1-1").added("1", "1-2");
+        multimapTree.added("2", "2-1");
+
+        multimapTree.removed("1");
+
+        assertFalse(multimapTree.contains("1"));
+        final Collection<String> rootChildren = multimapTree
+                .getChildren("root");
+        assertThat(2, is(rootChildren.size()));
+        assertFalse(rootChildren.contains("1"));
+        assertFalse(multimapTree.contains("1-2"));
+        assertNotSame("root", multimapTree.getParent("1"));
+    }
 
 }
