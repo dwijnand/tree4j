4bf226b0832d4ed24622046d4587ff7db7e04e06
Dale Wijnand <dale.wijnand@gmail.com>
2011-03-22 14:11:45 +0100

    remove MultimapTree as a specific type of MutableTree

--- src/test/java/com/dwijnand/tree/MutableTreeTest.java
+++ src/test/java/com/dwijnand/tree/MutableTreeTest.java
@@ -8,21 +8,42 @@ import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Collection;
+import java.util.Map;
 
 import org.junit.experimental.theories.DataPoints;
 import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
 
-import com.dwijnand.tree.MultimapTree;
-import com.dwijnand.tree.MutableTree;
+import com.google.common.base.Supplier;
+import com.google.common.collect.LinkedHashMultimap;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
 
 @RunWith(Theories.class)
 public class MutableTreeTest extends TreeTest {
 
     @DataPoints
     public static MutableTree<?>[] data() {
-        return new MutableTree[] {MultimapTree.<String> create()};
+        final Supplier<Multimap<String, String>> childrenMultimapSupplier = new Supplier<Multimap<String, String>>() {
+
+            @Override
+            public Multimap<String, String> get() {
+                return LinkedHashMultimap.create();
+            }
+
+        };
+
+        final Supplier<Map<String, String>> parentsMapSupplier = new Supplier<Map<String, String>>() {
+
+            @Override
+            public Map<String, String> get() {
+                return Maps.newHashMap();
+            }
+
+        };
+        return new MutableTree[] {BaseMutableTree.create(
+                childrenMultimapSupplier, parentsMapSupplier)};
     }
 
     @Theory
@@ -45,7 +66,7 @@ public class MutableTreeTest extends TreeTest {
 
     @Theory
     public void addedShouldAddTheParentChildAssociation(
-            final MultimapTree<String> multimapTree) {
+            final MutableTree<String> multimapTree) {
         multimapTree.setRoot("root");
 
         multimapTree.added("root", "child");
@@ -57,7 +78,7 @@ public class MutableTreeTest extends TreeTest {
 
     @Theory
     public void addedShouldThrowAnIllegalArgumentExceptionOnUnknownParent(
-            final MultimapTree<String> multimapTree) {
+            final MutableTree<String> multimapTree) {
         expectedException.expect(IllegalArgumentException.class);
         expectedException
                 .expectMessage("does not contain parent node unknown parent");
@@ -67,7 +88,7 @@ public class MutableTreeTest extends TreeTest {
 
     @Theory
     public void clearShouldRemoveAllNodes(
-            final MultimapTree<String> multimapTree) {
+            final MutableTree<String> multimapTree) {
         multimapTree.setRoot("root");
         multimapTree.added("root", "child");
 
@@ -79,7 +100,7 @@ public class MutableTreeTest extends TreeTest {
 
     @Theory
     public void removedShouldRemoveTheSpecifiedNodeAndAllOfItsChildren(
-            final MultimapTree<String> multimapTree) {
+            final MutableTree<String> multimapTree) {
         multimapTree.setRoot("root");
         multimapTree.added("root", "1").added("root", "2")
                 .added("root", "3");
