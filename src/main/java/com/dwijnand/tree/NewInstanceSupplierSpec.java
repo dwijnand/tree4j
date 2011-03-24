841f9efc172f04c4124bf6883bd75c952f93500c
Dale Wijnand <dale.wijnand@gmail.com>
2011-03-24 01:23:09 +0100

    Add NewInstanceSupplierSpec as an interface that extends and restricts the Supplier instance

---
+++ src/main/java/com/dwijnand/tree/NewInstanceSupplierSpec.java
@@ -0,0 +1,22 @@
+package com.dwijnand.tree;
+
+import com.google.common.base.Supplier;
+
+/**
+ * This interface extends the {@link Supplier} interface, restricting it to a
+ * supplier of only new instances of the appropriate type.
+ *
+ * @param <T> the type of the objects supplied
+ */
+public interface NewInstanceSupplierSpec<T> extends Supplier<T> {
+
+    // Override javadoc without inheriting to restrict
+    /**
+     * Retrieves a new instance of the appropriate type.
+     *
+     * @return an new instance
+     */
+    @Override
+    T get();
+
+}
