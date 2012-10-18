f8b9c7ec810dda14b08a7f12dcac693b080334b6
Dale Wijnand <dale.wijnand@gmail.com>
2012-10-19 01:52:53 +0200

    MutableMap/MutableMultimap: Fix a typo in a javadoc @link.

--- src/main/java/org/dapacode/tree4j/common/MutableMap.java
+++ src/main/java/org/dapacode/tree4j/common/MutableMap.java
@@ -4,10 +4,9 @@ import java.util.Map;
 
 /**
  * This interface extends the {@link Map} interface, adding the restriction that all optional (modifying) methods are implements
- * (i.e. don't throw {@link javax.naming.OperationNotSupportedException OperationNotSupportedException}).
+ * (i.e. don't throw {@link UnsupportedOperationException}).
  *
  * @param <K> the type of keys maintained by this map
  * @param <V> the type of mapped values
  */
-public interface MutableMap<K, V> extends Map<K, V> {
-}
+public interface MutableMap<K, V> extends Map<K, V> {}
--- src/main/java/org/dapacode/tree4j/common/MutableMultimap.java
+++ src/main/java/org/dapacode/tree4j/common/MutableMultimap.java
@@ -4,10 +4,9 @@ import com.google.common.collect.Multimap;
 
 /**
  * This interface extends the {@link Multimap} interface, adding the restriction that all optional (modifying) methods are
- * implements (i.e. don't throw {@link javax.naming.OperationNotSupportedException OperationNotSupportedException}).
+ * implements (i.e. don't throw {@link UnsupportedOperationException}).
  *
  * @param <K> the type of keys maintained by this multimap
  * @param <V> the type of mapped values
  */
-public interface MutableMultimap<K, V> extends Multimap<K, V> {
-}
+public interface MutableMultimap<K, V> extends Multimap<K, V> {}
