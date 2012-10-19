8850ba71505620a551a099cfde852d02130d2554
Dale Wijnand <dale.wijnand@gmail.com>
2012-10-19 03:35:39 +0200

    MultimapTree: Minor change & a TODO.

--- src/main/java/org/dapacode/tree4j/MultimapTree.java
+++ src/main/java/org/dapacode/tree4j/MultimapTree.java
@@ -139,8 +139,7 @@ public class MultimapTree<T> extends AbstractMultimapTree<T> implements MutableT
     checkNotNull(node);
     checkArgument(contains(node), "The tree doesn't contain the specified node: %s", node);
 
-    if (node == root) { // optimisation
-      root = null;
+    if (node == root) { // optimisation // TODO test this case
       clear();
       return true;
     }
