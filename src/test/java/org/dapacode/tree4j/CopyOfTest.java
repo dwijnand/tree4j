2f44cb9dd3481f16e527132ba7dd8852fac16875
Dale Wijnand <dale.wijnand@gmail.com>
2012-11-07 00:35:43 +0100

    tests/CopyOfTest: Rewrite tests to include both delegating implementations.

--- src/test/java/org/dapacode/tree4j/CopyOfTest.java
+++ src/test/java/org/dapacode/tree4j/CopyOfTest.java
@@ -1,30 +1,59 @@
 package org.dapacode.tree4j;
 
-import org.dapacode.tree4j.testutils.TreeHelper;
 import org.dapacode.tree4j.testutils.TreeHelper.Test;
-import org.junit.experimental.theories.DataPoints;
-import org.junit.experimental.theories.Theories;
-import org.junit.experimental.theories.Theory;
-import org.junit.runner.RunWith;
 
+import static org.dapacode.tree4j.testutils.TreeHelper.setupTreeTestData;
 import static org.dapacode.tree4j.testutils.TreeHelper.withoutModifying;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
-@RunWith(Theories.class)
 // CSOFF: WhitespaceAroundCheck
 @SuppressWarnings({"LocalCanBeFinal", "InstanceMethodNamingConvention", "DesignForExtension"})
 // CSON: WhitespaceAroundCheck
 public class CopyOfTest {
-  @DataPoints
-  public static Tree<?>[] data() {
-    return new Tree<?>[]{MultimapTree.create(), ImmutableMultimapTree.create()};
+  @org.junit.Test
+  public void immutableMultimapTreeCopyOfShouldReturnAnEqualButNotSameTreeOnMultimapTree() {
+    testImmutableMultimapTreeCopyOf(setupTreeTestData(MultimapTree.<String>create()));
   }
 
-  @Theory
-  public void immutableMultimapTreeCopyOfShouldReturnAnEqualButNotSameTree(Tree<String> tree) {
-    tree = setupTreeTestData(tree);
+  @org.junit.Test
+  public void immutableMultimapTreeCopyOfShouldReturnAnEqualButNotSameTreeOnImmutableMultimapTree() {
+    testImmutableMultimapTreeCopyOf(setupTreeTestData(ImmutableMultimapTree.<String>create()));
+  }
 
+  @org.junit.Test
+  public void multimapTreeCopyOfShouldReturnAnEqualButNotSameTreeOnMultimapTree() {
+    testMultimapTreeCopyOf(setupTreeTestData(MultimapTree.<String>create()));
+  }
+
+  @org.junit.Test
+  public void multimapTreeCopyOfShouldReturnAnEqualButNotSameTreeOnImmutableMultimapTree() {
+    testMultimapTreeCopyOf(setupTreeTestData(ImmutableMultimapTree.<String>create()));
+  }
+
+  @org.junit.Test
+  public void immutableMultimapTreeCopyOfShouldReturnAnEqualButNotSameTreeOnDelegatingImmutableTree() {
+    final ImmutableTree<String> delegate = setupTreeTestData(ImmutableMultimapTree.<String>create());
+    testImmutableMultimapTreeCopyOf(new DelegatingImmutableTree<String>() {
+      @Override
+      protected ImmutableTree<String> delegate() {
+        return delegate;
+      }
+    });
+  }
+
+  @org.junit.Test
+  public void multimapTreeCopyOfShouldReturnAnEqualButNotSameTreeOnDelegatingMutableTree() {
+    final MutableTree<String> delegate = setupTreeTestData(MultimapTree.<String>create());
+    testMultimapTreeCopyOf(new DelegatingMutableTree<String>() {
+      @Override
+      protected MutableTree<String> delegate() {
+        return delegate;
+      }
+    });
+  }
+
+  private void testImmutableMultimapTreeCopyOf(Tree<String> tree) {
     withoutModifying(tree, new Test<Tree<String>>() {
       @Override
       public void apply(Tree<String> tree) {
@@ -35,10 +64,7 @@ public class CopyOfTest {
     });
   }
 
-  @Theory
-  public void multimapTreeCopyOfShouldReturnAnEqualButNotSameTree(Tree<String> tree) {
-    tree = setupTreeTestData(tree);
-
+  private void testMultimapTreeCopyOf(Tree<String> tree) {
     withoutModifying(tree, new Test<Tree<String>>() {
       @Override
       public void apply(Tree<String> tree) {
@@ -47,15 +73,5 @@ public class CopyOfTest {
         assertThat(copyTree, is(equalTo(tree)));
       }
     });
-  }
-
-  private Tree<String> setupTreeTestData(Tree<String> tree) {
-    if (tree instanceof ImmutableTree) {
-      return TreeHelper.setupTreeTestData((ImmutableTree<String>) tree);
-    } else if (tree instanceof MultimapTree) {
-      return TreeHelper.setupTreeTestData((MultimapTree<String>) tree);
-    } else {
-      throw new AssertionError("Unexpected Tree type: " + tree.getClass());
-    }
   }
 }
